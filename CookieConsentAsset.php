fa9334bc8c2e969cb11a653d1a4eb536c8e31293
easyrider7522 <easyrider7522@gmail.com>
2018-02-12 17:52:56 +0100

    Add files via upload

---
+++ CookieConsentAsset.php
@@ -0,0 +1,24 @@
+<?php
+namespace easyrider7522\cookieconsent;
+
+use yii\web\AssetBundle;
+
+
+class CookieConsentAsset extends AssetBundle
+{
+	public $sourcePath = '@bower/cookieconsent3/build/';
+
+	public $css = [
+		// '//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css',
+		'cookieconsent.min.css',
+	];
+
+	public $js= [
+		// '//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js',
+		'cookieconsent.min.js',
+	];
+
+	public $jsOptions= [
+		'position'	=> 'POS_HEAD',
+	];
+}

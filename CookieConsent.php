c9d2ad726c8381d3438e6c1c670b16673a633eef
easyrider7522 <easyrider7522@gmail.com>
2018-02-13 16:43:36 +0100

    Update CookieConsent.php

--- CookieConsent.php
+++ CookieConsent.php
@@ -8,25 +8,9 @@ class CookieConsent extends Widget
 {
     public $lang = 'en';
     
-    public $options = [
-        'palette'   => [
-            'popup'     => [
-                'background'    => '#237afc',
-            ],
-            'button'    => [
-                'background'    => '#fff',
-                'text'          => '#237afc',
-            ],
-        ],
-        'theme'     => 'classic',
-        'position'  => 'bottom-right',
-        'content'   => [
-            'message'   => 'Ez az oldal is sütiket használ, aminek a tényét az EU-s törvények alapján jelezni kell a felhasználók felé.',
-            'dismiss'   => 'Értem',
-            'link'      => 'Sütikről',
-            'href'      => 'https://hu.wikipedia.org/wiki/HTTP-s%C3%BCti',
-        ],
-    ];
+    public $options = [];
+    
+    public $optionsJs = false;
     
     
     /**
@@ -45,9 +29,11 @@ class CookieConsent extends Widget
     {
         $view = $this->getView();
         
-        $code = 'window.addEventListener("load",function(){window.cookieconsent.initialise(' . json_encode( $this->options ) . ')});';
+        $jsObject = $this->optionsJs ?
+                    $this->optionsJs :
+                    ( empty( $this->options ) ? '{}' : json_encode( $this->options ));
         
-        $view->registerJs( $code, $view::POS_HEAD, 'cookieconsent-start' );
+        $view->registerJs( "window.addEventListener(\"load\",function(){window.cookieconsent.initialise({$jsObject})});", $view::POS_HEAD, 'cookieconsent-starter' );
         
         CookieConsentAsset::register( $view );
     }
